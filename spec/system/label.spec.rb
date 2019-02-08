42a8b4bead369f01c435be61f3fbf5caa54828e5
YukiTamura <yuki_tamura@diveintocode.jp>
2019-02-09 02:09:40 +0900

    インデント修正

--- spec/system/label.spec.rb
+++ spec/system/label.spec.rb
@@ -1,30 +1,29 @@
 require 'rails_helper'
 
 describe "ラベル機能", type: :system do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
 
-    
-    let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', detail: '最初の詳細', expired_at: '2018-12-31', status: '未着手', priority: '高', user: user_a ) }
-    let!(:task_b) { FactoryBot.create(:task, name: '三番目のタスク', detail: '三番目の詳細', expired_at: '2018-12-31', status: '未着手', priority: '高', user: user_a ) }
-    
-    let!(:label_1) { FactoryBot.create(:label, name: 'ラベル1') }
-    let!(:label_2) { FactoryBot.create(:label, name: 'ラベル2') }
-   
-    let!(:labeling_1) { FactoryBot.create(:labeling, task: task_a, label: label_1) }
-    let!(:labeling_2) { FactoryBot.create(:labeling, task: task_b, label: label_2) }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', detail: '最初の詳細', expired_at: '2018-12-31', status: '未着手', priority: '高', user: user_a ) }
+  let!(:task_b) { FactoryBot.create(:task, name: '三番目のタスク', detail: '三番目の詳細', expired_at: '2018-12-31', status: '未着手', priority: '高', user: user_a ) }
 
+  let!(:label_1) { FactoryBot.create(:label, name: 'ラベル1') }
+  let!(:label_2) { FactoryBot.create(:label, name: 'ラベル2') }
+
+  let!(:labeling_1) { FactoryBot.create(:labeling, task: task_a, label: label_1) }
+  let!(:labeling_2) { FactoryBot.create(:labeling, task: task_b, label: label_2) }
+
+  before do
+    visit login_path
+    fill_in 'Email', with: 'a@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'ログインする'
+    expect(page).to have_content "ユーザーAタスク一覧"
+  end
+
+  describe "ラベル一覧のテスト" do
     before do
-      visit login_path
-      fill_in 'Email', with: 'a@example.com'
-      fill_in 'Password', with: 'password'
-      click_button 'ログインする'
-      expect(page).to have_content "ユーザーAタスク一覧"
+      visit labels_path
     end
-
-    describe "ラベル一覧のテスト" do
-      before do
-        visit labels_path
-      end
 
     it "作成したラベル一覧が表示される" do
       expect(page).to have_content 'ラベル1'
@@ -51,6 +50,7 @@ describe "ラベル機能", type: :system do
     before do
       visit edit_label_path(label_1)
     end
+
     it "ラベルを編集する" do
       fill_in 'Name', with: 'ラベル4'
       click_on '更新する'
@@ -68,6 +68,7 @@ describe "ラベル機能", type: :system do
       expect(page).to have_content '最初のタスク'
       expect(page).to have_content '三番目のタスク'
     end
+
     it "ラベル1がラベリングされているタスクを表示する" do
       select 'ラベル1', from: 'task_label_id'
       click_on '検索'
