61a2f904f4b5c5470a085a57599d2a62dc451bd1
YukiTamura <yuki_tamura@diveintocode.jp>
2019-02-09 01:59:22 +0900

    ラベルテスト追記、タスクテストが落ちたので修正

--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -1,24 +1,34 @@
 require 'rails_helper'
 
 describe "タスク管理機能", type: :system do
-  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', detail: '最初の詳細', expired_at: '2018-12-31', status: '未着手', priority: '高')}
-  let!(:task_b) { FactoryBot.create(:task, name: '二番目のタスク', detail: '二番目の詳細', expired_at: '2018-12-24', status: '着手中', priority: '中')}
-  let!(:task_c) { FactoryBot.create(:task, name: '三番目のタスク', detail: '三番目の詳細', expired_at: '2018-12-16', status: '完了', priority: '中')}
-  let!(:task_d) { FactoryBot.create(:task, name: '四番目のタスク', detail: '四番目の詳細', expired_at: '2019-01-01', status: '完了', priority: '低')}
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', detail: '最初の詳細', expired_at: '2018-12-31', status: '未着手', priority: '高', user: user) }
+  let!(:task_b) { FactoryBot.create(:task, name: '二番目のタスク', detail: '二番目の詳細', expired_at: '2018-12-24', status: '着手中', priority: '中', user: user) }
+  let!(:task_c) { FactoryBot.create(:task, name: '三番目のタスク', detail: '三番目の詳細', expired_at: '2018-12-16', status: '完了', priority: '中', user: user) }
+  let!(:task_d) { FactoryBot.create(:task, name: '四番目のタスク', detail: '四番目の詳細', expired_at: '2019-01-01', status: '完了', priority: '低', user: user) }
 
-   describe 'タスク作成のテスト' do
-     before do
-       visit new_task_path
-       fill_in 'タスク名', with: '夕ご飯の買い物'
-       fill_in '詳しい内容', with: '玉ねぎ'
-       click_on '登録する'
-     end
-    # save_and_open_page
-    it '作成したタスクが表示される' do
-      expect(page).to have_content '夕ご飯の買い物'
-      expect(page).to have_content '玉ねぎ'
+  before do
+    visit login_path
+    fill_in 'Email', with: 'test1@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'ログインする'
+    expect(page).to have_content "テストユーザータスク一覧"
+  end
+
+  describe 'タスク作成のテスト' do
+    before do
+      visit new_task_path
+      fill_in 'タスク名', with: '夕ご飯の買い物'
+      fill_in '詳しい内容', with: '玉ねぎ'
+      fill_in '終了期限', with: '2019-02-28'
+      click_on '登録する'
     end
-   end
+  # save_and_open_page
+  it '作成したタスクが表示される' do
+    expect(page).to have_content '夕ご飯の買い物'
+    expect(page).to have_content '玉ねぎ'
+  end
+  end
 
   describe 'タスク一覧のテスト' do
     before do
@@ -60,15 +70,15 @@ describe "タスク管理機能", type: :system do
     let(:tasks) { page.all('tbody tr') }
     before do
       visit tasks_path
+      # save_and_open_page
+      click_link '終了期限'
       sleep 1
-      click_link '終了期限でソートする'
     end
     it '終了期限の降順で表示される' do
       expect(tasks[0]).to have_content '2019-01-01'
       expect(tasks[1]).to have_content '2018-12-31'
       expect(tasks[2]).to have_content '2018-12-24'
       expect(tasks[3]).to have_content '2018-12-16'
-      #save_and_open_page
     end
   end
 
@@ -76,15 +86,14 @@ describe "タスク管理機能", type: :system do
     let(:tasks) { page.all('tbody tr') }
     before do
       visit tasks_path
+      click_link '優先度'
       sleep 1
-      click_link '優先順位でソートする'
     end
     it '優先度の高い順で表示される' do
       expect(tasks[0]).to have_content '高'
       expect(tasks[1]).to have_content '中'
       expect(tasks[2]).to have_content '中'
       expect(tasks[3]).to have_content '低'
-      # save_and_open_page
     end
   end
 end
